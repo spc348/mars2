ed7fd8adc519c0985db9360bbe6e55d2d219fb3a
cahn <spc348@nyu.edu>
2019-11-06 00:49:21 -0500

    added some cleanup to the opcodes

--- src/hw2/Operands/Instruction.java
+++ src/hw2/Operands/Instruction.java
@@ -28,6 +28,7 @@ public class Instruction {
     String[] jTypeNames = {"Opcode", "Address"};
     private int rsValue = 0;
     private int immAddress;
+    private int rtValue;
 
     public Instruction(String str, JTable registerTable, JTable memoryTable) {
         this.immAddress = 0;
@@ -125,10 +126,12 @@ public class Instruction {
                 String rawMemory = memoryTable.getModel().getValueAt((int) operand.getSource1(), MainDisplay.MEMORY_TABLE_VALUE).toString();
                 operand.setSource1(Integer.parseInt(rawMemory));
             } else {
-                String rawMemory = registerTable.getModel().getValueAt((int) operand.getSource1(), MainDisplay.REGISTER_TABLE_VALUE).toString();
+                rsValue = (int) operand.getSource1();
+                String rawMemory = registerTable.getModel().getValueAt(rsValue, MainDisplay.REGISTER_TABLE_VALUE).toString();
                 operand.setSource1(Integer.parseInt(rawMemory));
                 if (!operand.hasOneSource()) {
-                    rawMemory = registerTable.getModel().getValueAt((int) operand.getSource2(), MainDisplay.REGISTER_TABLE_VALUE).toString();
+                    rtValue = (int) operand.getSource2();
+                    rawMemory = registerTable.getModel().getValueAt(rtValue, MainDisplay.REGISTER_TABLE_VALUE).toString();
                     operand.setSource2(Integer.parseInt(rawMemory));
                 }
             }
@@ -151,37 +154,23 @@ public class Instruction {
 
     public String getInstructionString() {
         StringBuilder builder = new StringBuilder();
-        builder.append(String.format("%s: ", rTypeNames[0]));
         builder.append(Integer.toBinaryString(opcode));
         if (null != getInstructionType()) {
             switch (getInstructionType()) {
                 case J:
-                    builder.append(String.format(" %s: ", jTypeNames[1]));
-                    builder.append(String.format("%025d", immAddress));
+                    builder.append(String.format(" %026d ", immAddress));
                     break;
                 case R:
-                    builder.append(String.format(" %s: ", rTypeNames[1])); //rs
-                    builder.append("\t");
-                    builder.append(String.format(" %s: ", rTypeNames[2])); //rt
-
-                    builder.append("\t");
-                    builder.append(String.format(" %s: ", rTypeNames[3])); //rd
-                    builder.append(String.format("%d", destination));
-                    builder.append("\t");
-
-                    builder.append(String.format(" %s: ", rTypeNames[4]));
-                    builder.append("\t");
-                    builder.append(String.format(" %s: ", rTypeNames[5]));
+                    builder.append(String.format(" %05d ", rsValue));
+                    builder.append(String.format(" %05d ", rtValue));
+                    builder.append(String.format(" %05d ", destination));
+                    builder.append(String.format(" %05d ", 0));
+                    builder.append(String.format(" %06d ", 0));
                     break;
                 case I:
-                    builder.append(String.format(" %s: ", iTypeNames[1])); //rs
-                    builder.append(String.format("%d", destination)); //rs
-                    builder.append("\t");
-                    builder.append(String.format(" %s: ", iTypeNames[2]));
-                    builder.append(String.format(" %05d: ", rsValue)); //rs
-                    builder.append("\t");
-                    builder.append(String.format(" %s: ", iTypeNames[3]));
-                    builder.append(String.format("%016d", immAddress));
+                    builder.append(String.format(" %06d ", destination));
+                    builder.append(String.format(" %05d ", rsValue));
+                    builder.append(String.format(" %016d ", immAddress));
                     break;
                 default:
                     break;
